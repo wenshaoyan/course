1adf6d41cd8737d55cec40a759c713c8f4b4a776
wenshao <wenshao314106@163.com>
2017-10-18 22:26:20 +0800

    init

---
+++ course-client/course_admin_vue/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app-2">
+  <span>
+    {{message}}
+  </span>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      message: '页面加载于 ' + new Date().toLocaleString()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
