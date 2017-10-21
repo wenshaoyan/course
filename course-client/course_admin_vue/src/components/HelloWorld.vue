7a4a40bb1bb905e3b87b3d2ac0d7ac748c547ed5
wenshao <wenshao314106@163.com>
2017-10-21 18:22:54 +0800

    t

--- course-client/course_admin_vue/src/components/HelloWorld.vue
+++ course-client/course_admin_vue/src/components/HelloWorld.vue
@@ -1,41 +1,41 @@
 <template>
-  <div id="app-2">
-  <span>
-    {{message}}
-  </span>
-  </div>
-
+  <section>
+    <mu-appbar title="后台管理" :zDepth="3">
+      <mu-icon value="settings_input_svideo" color="#f48fb1" slot="left">
+      </mu-icon>
+      <mu-chip backgroundColor="rgba(0, 0, 0, 0)" slot="right">
+        <mu-avatar src="https://avatars1.githubusercontent.com/u/22613580?v=4&s=40" :size="32">
+        </mu-avatar><span class="bar-name">wenshao</span>
+      </mu-chip>
+    </mu-appbar>
+    <mu-row>
+      <mu-col width="20" tablet="20" desktop="20">
+        &nbsp;
+      </mu-col>
+      <mu-col width="20" tablet="20" desktop="20">
+        &nbsp;
+      </mu-col>
+    </mu-row>
+  </section>
 </template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      message: '页面加载于 ' + new Date().toLocaleString()
-    }
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .bar-name {
+    color: white;
+  }
+  .row-left {
+    border-right: 1px dashed rgba(0, 0, 0, 0.1);
+  }
+  .menu-active {
+    border-left: 2px solid lightblue;
+  }
+  .board-left,
+  .board-right {
+    padding: 5px;
+  }
+  .paper-draft {
+    padding: 10px;
+  }
+  .list-menu {
+    background: #eeeeee;
+  }
 </style>
