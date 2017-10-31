a083c7b396b15b1b5c10125e996301f11a6def70
wenshao <wenshao314106@163.com>
2017-10-31 20:25:35 +0800

    删除test

--- course-client/course_admin_vue/src/router/index.js
+++ course-client/course_admin_vue/src/router/index.js
@@ -78,8 +78,7 @@ export const asyncRouterMap = [
     name: 'app管理',
     icon: 'theme',
     children: [
-      { path: 'banner', component: _import('app/banner'), name: '轮播图' },
-      { path: 'test', component: _import('app/test'), name: '轮播图' }
+      { path: 'banner', component: _import('app/banner'), name: '轮播图' }
     ]
   },
   {
