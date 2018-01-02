3657ead8c9d1f4b0faa7e55a707f8a5d3ff0bd93
yanshaowen <yanshaowen@zhiketong.cn>
2018-01-02 19:11:11 +0800

    增加AllServer

--- course-dal/DAL/src/main/java/com/wenshao/dal/server/AllServer.java
+++ course-dal/DAL/src/main/java/com/wenshao/dal/server/AllServer.java
@@ -1,17 +1,5 @@
 package com.wenshao.dal.server;
 
-import com.wenshao.dal.handler.DynamicCacheProxyHandler;
-import com.wenshao.dal.handler.UserHandler;
-import com.wenshao.dal.thriftgen.UserService;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
-import java.io.IOException;
-import java.io.Reader;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-
 /**
  * Created by wenshao on 2017/9/4
  * 所有服务 解决内存不足
